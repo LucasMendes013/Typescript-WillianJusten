5b8d7f792ef72179c415ff6d048121d7deaba83e
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-06-29 16:08:26 -0300

    07

--- types.ts
+++ types.ts
@@ -61,3 +61,24 @@ let message2 = 'mensagem indefinida'
 message2 =  'string' 
 
 //assim como, ao dizer que algo é string, nao se pode usar metoodos e propriedades fora de string
+
+
+
+//Type Alias(varios tipos em um local apenas)
+//Caso voce queira estipular que uma propriedade de uma function por exemplo pode ser number ou string, coloque:   : type | type    |
+function doisTipos(tipo1: string | number, tipo2: string | number) {
+  console.log(`Tipo 1 é ${tipo1} e tipo 2 é ${tipo2}`)
+}
+doisTipos('ola','mundo')
+doisTipos(1,2)
+
+
+//Usando o type Alias, estipulamos que ele é string e também que ele recebe apenas um desses valores, se o usuario colocar 'ios' no na execucao da funcion,  nao funcionara, mesmo que o tipo esteja correto, a informacao dentro dele nao está
+
+type Plataform = 'windows' | 'linux' | 'Mac Os'
+//è interessante também, que como o TS jáa sabe que plataform é string, nao precisamos colocar isso novamente,basta dizer que é :
+function renderPlataform(plataform : Plataform){
+  return plataform
+}
+
+renderPlataform('')
